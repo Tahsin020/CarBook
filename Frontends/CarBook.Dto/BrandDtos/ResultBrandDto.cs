db41c46ddb022e9381cf65644eac12ed2cc15c22
Thsn020 <Ithsn8@gmail.com>
2024-08-20 19:40:17 +0300

    Create car

---
+++ Frontends/CarBook.Dto/BrandDtos/ResultBrandDto.cs
@@ -0,0 +1,6 @@
+﻿namespace CarBook.Dto.BrandDtos;
+public sealed class ResultBrandDto
+{
+    public int Id { get; set; }
+    public string Name { get; set; }
+}
--- Frontends/CarBook.WebUI/Controllers/AdminCarController.cs
+++ Frontends/CarBook.WebUI/Controllers/AdminCarController.cs
@@ -1,5 +1,7 @@
-﻿using CarBook.Dto.CarDtos;
+﻿using CarBook.Dto.BrandDtos;
+using CarBook.Dto.CarDtos;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
 using Newtonsoft.Json;
 
 namespace CarBook.WebUI.Controllers;
@@ -26,8 +28,14 @@ public class AdminCarController : Controller
     }
 
     [HttpGet]
-    public IActionResult CreateCar()
+    public async Task<IActionResult> CreateCar()
     {
+        var client = _httpClientFactory.CreateClient();
+        var responseMessage = await client.GetAsync("https://localhost:7209/api/Brands");
+        var jsonData = await responseMessage.Content.ReadAsStringAsync();
+        var values = JsonConvert.DeserializeObject<List<ResultBrandDto>>(jsonData);
+        List<SelectListItem> brandListItems = (from x in values select new SelectListItem { Text = x.Name, Value = x.Id.ToString() }).ToList();
+        ViewBag.BrandValues = brandListItems;
         return View();
     }
 }
